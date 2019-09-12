f69358c884f8e8f5bab8fc54579f42e1153daede
Jim Bair <james.d.bair@gmail.com>
2019-09-12 13:20:51 -0500

    Got tired of re-installing grub after every bios update from gigabyte

---
+++ grubfix.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Because Gigabyte nukes my grub install with each firmware update on my
+# X570 I AORUS PRO WIFI board, and my 3900X seems to need many of those.
+mount /dev/nvme0n1p7 /mnt/gentoo
+mount /dev/nvme0n1p6 /mnt/gentoo/boot
+cp --dereference /etc/resolv.conf /mnt/gentoo/etc/
+mount --types proc /proc /mnt/gentoo/proc
+mount --rbind /sys /mnt/gentoo/sys
+mount --make-rslave /mnt/gentoo/sys
+mount --rbind /dev /mnt/gentoo/dev
+mount --make-rslave /mnt/gentoo/dev
+chroot /mnt/gentoo mount /boot
+chroot /mnt/gentoo grub-install --target=x86_64-efi --efi-directory=/boot 
+umount -l /mnt/gentoo/dev{/shm,/pts,}
+umount -R /mnt/gentoo
+reboot
