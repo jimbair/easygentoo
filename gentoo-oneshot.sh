4ab06f4f4a98dc73bdcd90db5797aa5b1f23252d
Jim Bair <james.d.bair@gmail.com>
2021-07-03 16:51:40 -0500

    eselect is required even with a single kernel

--- gentoo-oneshot.sh
+++ gentoo-oneshot.sh
@@ -156,6 +156,7 @@ emerge --autounmask-write sys-kernel/gentoo-sources sys-kernel/genkernel
 etc-update --automode -5
 echo "sys-kernel/linux-firmware @BINARY-REDISTRIBUTABLE" >> /etc/portage/package.license
 emerge sys-kernel/gentoo-sources sys-kernel/genkernel
+eselect kernel set 1
 genkernel all --makeopts=-j$(grep -c processor /proc/cpuinfo)
 emerge --noreplace net-misc/netifrc
 echo 'hostname="gentoo"' > /etc/conf.d/hostname
