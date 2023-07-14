a87e01c82cbc60c880575b09a2b00d1f87780f71
Jim Bair <james.d.bair@gmail.com>
2023-07-14 11:42:50 -0500

    Check if we are in EFI mode before running

--- grubfix.sh
+++ grubfix.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 # Because Gigabyte nukes my grub install with each firmware update on my
 # X570 I AORUS PRO WIFI board, and my 5950X seems to need many of those.
+
+# USB drive can boot either mode; be sure we're in EFI mode
+if [[ ! -d '/sys/firmware/efi/' ]]; then
+    echo "ERROR: This system is not in EFI Mode. Exiting." >&2
+    exit 1
+fi
+
+# Do the fixin
 mount /dev/nvme1n1p7 /mnt/gentoo
 mount /dev/nvme1n1p6 /mnt/gentoo/boot
 cp --dereference /etc/resolv.conf /mnt/gentoo/etc/
